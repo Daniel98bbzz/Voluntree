8e332fad6c3d5f4cede365d549caebc280750b68
nir itah <127130321+niritah123@users.noreply.github.com>
2024-03-04 23:39:29 +0200

    Delete pages directory

--- pages/_app.js
+++
@@ -1,6 +0,0 @@
-import "../styles/globals.css";
-const { default: mongoose } = require("mongoose");
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
