86f105bbe89aec7e729b0d7a8e3af7f3bda932f8
Daniel98bbzz <102686490+Daniel98bbzz@users.noreply.github.com>
2024-03-05 00:23:11 +0200

    handlers

---
+++ src/api/opportunities.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import opportunityModel from "../../../models/opportunityModel";
+
+export default async function handler(req, res) {
+  let client = await mongoose.connect(process.env.DATABASE_URL);
+  console.log(client);
+
+  if (req.method === "GET") {
+    const opportunities = await opportunityModel.find();
+    res.status(200).json({ data: JSON.stringify(opportunities) });
+  }
+
+  res.status(400).json({ error: "No Method" });
+}
